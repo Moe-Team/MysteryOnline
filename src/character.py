040a8f16ddf1df13bd9cf1d40d37bfaec44db715
MurdockHM <mmurdockh@gmail.com>
2017-04-08 19:02:20 +0200

    Some ini and character work

--- src/character.py
+++ src/character.py
@@ -1,9 +1,20 @@
 from kivy.config import ConfigParser
+from kivy.atlas import Atlas
 
 
 class Character:
 
     def __init__(self, name):
         self.name = name
+        self.path = "characters/{0}/".format(self.name)
         self.config = ConfigParser(self.name)
-        self.config.read("characters/{0}/settings.ini".format(self.name))
+        self.config.read(self.path + "settings.ini")
+        char = self.config['character']
+        self.display_name = char['name']
+        self.series = char['series']
+        self.sprites_path = self.path + char['sprites']
+        self.icons_path = self.path + char['icons']
+
+    def load(self):
+        self.sprites = Atlas(sprites_path)
+        self.icons = Atlas(icons_path)
