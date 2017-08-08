478341e952beb9e77d417fd94c093587ccf44bb6
MurdockHM <mmurdockh@gmail.com>
2017-08-08 16:11:47 +0200

    Kind of fixed the character select positioning

--- src/character_select.py
+++ src/character_select.py
@@ -4,7 +4,9 @@ from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.properties import ObjectProperty
+from kivy.graphics import Color, Rectangle
 from character import characters, series_list
+from math import ceil
 
 
 class CharacterToggle(ToggleButton):
@@ -31,14 +33,15 @@ class CharacterSelect(Popup):
         self.picked_char = None
         grids = {}
         for s in series_list:
-            self.main_lay.add_widget(Label(text=s, size_hint=(1, None), height=30))
-            grids[s] = GridLayout(cols=7, size_hint=(1, None), row_default_height=60, row_force_default=True)
+            self.main_lay.add_widget(Label(text=s, size_hint=(1, None)))
+            grids[s] = GridLayout(cols=7, size_hint=(1, None))
             self.main_lay.add_widget(grids[s])
 
         for g in grids:
             chars = list(filter(lambda x: x.series == g, characters.values()))
+            chars = sorted(chars, key=lambda x: x.name)
             for c in chars:
-                btn = CharacterToggle(c, group='char')
+                btn = CharacterToggle(c, group='char', size_hint=(1, None), height=60)
                 btn.bind(on_press=self.character_chosen)
                 grids[g].add_widget(btn)
         ok_btn = Button(text="OK", size_hint=(1, None), height=40, pos_hint={'y': 0, 'x': 0})
