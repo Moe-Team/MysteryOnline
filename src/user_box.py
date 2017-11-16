b9d79170c7412d561213f646c8cb2d5fe0eae28e
MurdockHM <mmurdockh@gmail.com>
2017-11-16 17:24:41 +0100

    Temporarily disable UserBox's tooltip

--- src/user_box.py
+++ src/user_box.py
@@ -1,38 +1,42 @@
 from kivy.properties import ObjectProperty
 from kivy.uix.modalview import ModalView
+from kivy.uix.widget import Widget
 
-from tooltip import TooltipBehavior
-import gc
+# from tooltip import TooltipBehavior
+# import gc
 
 
-class UserBox(TooltipBehavior):
+class UserBox(Widget):
     lbl = ObjectProperty(None)
     pm = ObjectProperty(None)
     mute = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super(UserBox, self).__init__(**kwargs)
-        self.popup = UserBoxPopup()
-        self.char_lbl_text = ""
-        self.sub_lbl_text = ""
-        self.loc_lbl_text = ""
-
-    def set_new_popup(self):
-        gc.collect()
-        del self.popup
-        self.popup = UserBoxPopup()
-        self.popup.char_lbl.text = self.char_lbl_text
-        self.popup.sub_lbl.text = self.sub_lbl_text
-        self.popup.loc_lbl.text = self.loc_lbl_text
+    #     self.popup = UserBoxPopup()
+    #     self.char_lbl_text = ""
+    #     self.sub_lbl_text = ""
+    #     self.loc_lbl_text = ""
+    #
+    # def set_new_popup(self):
+    #     gc.collect()
+    #     del self.popup
+    #     self.popup = UserBoxPopup()
+    #     self.popup.char_lbl.text = self.char_lbl_text
+    #     self.popup.sub_lbl.text = self.sub_lbl_text
+    #     self.popup.loc_lbl.text = self.loc_lbl_text
 
     def set_char_label(self, text):
-        self.char_lbl_text = text
+        # self.char_lbl_text = text
+        pass
 
     def set_sub_label(self, text):
-        self.sub_lbl_text = text
+        # self.sub_lbl_text = text
+        pass
 
     def set_loc_label(self, text):
-        self.loc_lbl_text = text
+        # self.loc_lbl_text = text
+        pass
 
 
 class UserBoxPopup(ModalView):
