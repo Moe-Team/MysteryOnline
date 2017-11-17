7e23071e6100f8831b450338d2023114c422660e
MurdockHM <mmurdockh@gmail.com>
2017-11-17 17:10:44 +0100

    Add validation to validator.py
    Usage: hashing: h folder output_file.json
    validating: v hashed_file1.json hashed_file2.json

--- validator.py
+++ validator.py
@@ -30,6 +30,42 @@ def hash_files_in_folder(folder, json_file):
         json.dump(hashed_files, f)
 
 
-input_folder = sys.argv[1]
-output_file = sys.argv[2]
-hash_files_in_folder(input_folder, output_file)
+def validate_hashes(file1, file2):
+    changed_files = []
+    missing_files = []
+    with open(file1, 'r') as f:
+        hashes1 = json.load(f)
+    with open(file2, 'r') as f:
+        hashes2 = json.load(f)
+
+    missing_files.append("{}:".format(file2))
+    for file in hashes1:
+        temp = hashes2.get(file, None)
+        if temp is None:
+            missing_files.append(file)
+    missing_files.append("{}:".format(file1))
+    for file in hashes2:
+        temp = hashes1.get(file, None)
+        if temp is None:
+            missing_files.append(file)
+
+    for file in hashes1:
+        old = hashes1[file]
+        new = hashes2.get(file, None)
+        if new is not None and old != new:
+            changed_files.append(file)
+    return changed_files, missing_files
+
+
+if sys.argv[1] == 'h':
+    input_folder = sys.argv[2]
+    output_file = sys.argv[3]
+    hash_files_in_folder(input_folder, output_file)
+elif sys.argv[1] == 'v':
+    changed_result, missing_result = validate_hashes(sys.argv[2], sys.argv[3])
+    with open("validator_changed_files.json", "w") as fil:
+        json.dump(changed_result, fil)
+    with open("validator_missing_files.json", "w") as fil:
+        json.dump(missing_result, fil)
+else:
+    print("Unrecognized command, aborting")
