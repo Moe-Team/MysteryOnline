1c28d94ab5cfdffa61d218140449ab818dd10e61
MurdockHM <mmurdockh@gmail.com>
2018-02-11 16:43:13 +0100

    Fix #13

--- src/left_tab.py
+++ src/left_tab.py
@@ -9,6 +9,45 @@ from kivy.clock import Clock
 from kivy.uix.scrollview import ScrollView
 
 
+class TrackSection:
+
+    def __init__(self, name):
+        self.name = name
+        self.subsections = {}
+        self.tracks = []
+
+    def add_subsection(self, subsection):
+        self.subsections[subsection.get_name()] = subsection
+
+    def add_track(self, track_name):
+        self.tracks.append(track_name)
+
+    def get_name(self):
+        return self.name
+
+    def get_subsections(self):
+        return self.subsections
+
+    def get_tracks(self):
+        return self.tracks
+
+
+class TrackSubSection:
+
+    def __init__(self, name):
+        self.name = name
+        self.tracks = []
+
+    def add_track(self, track_name):
+        self.tracks.append(track_name)
+
+    def get_name(self):
+        return self.name
+
+    def get_tracks(self):
+        return self.tracks
+
+
 class TrackLabel(Label):
 
     def __init__(self, **kwargs):
@@ -36,6 +75,8 @@ class SearchResults(ScrollView):
 
     def __init__(self, **kwargs):
         super(SearchResults, self).__init__(**kwargs)
+        self.scroll_type = ['bars']
+        self.bar_width = 10
 
     def add_label(self, label):
         self.children[0].add_widget(label)
@@ -52,9 +93,16 @@ class MusicList(TabbedPanelItem):
         super(MusicList, self).__init__(**kwargs)
         self.temp_data = []
         self.tracks = {}
-        self.track_list = []
+        self.search_space = []
+        self.track_search_space = []
+        self.section_search_space = []
+        self.subsection_search_space = []
+        self.sections = {}
+        self.subsections = {}
         self.search_results = SearchResults()
         self.search_done = False
+        self.current_section = None
+        self.current_subsection = None
 
     def ready(self):
         self.load_tracks()
@@ -69,16 +117,28 @@ class MusicList(TabbedPanelItem):
             Logger.warning('Music: musiclist.txt not found')
             return
         self.music_list_view.data = self.temp_data
-        self.track_list = list(self.tracks.keys())
-        self.track_list.sort(key=str.lower)
+        self.track_search_space = list(self.tracks.keys())
+        self.section_search_space = list(self.sections.keys())
+        self.subsection_search_space = list(self.subsections.keys())
+        self.track_search_space.sort(key=str.lower)
+        self.section_search_space.sort(key=str.lower)
+        self.subsection_search_space.sort(key=str.lower)
 
     def build_from_line(self, line):
         if line.startswith('['):
             section = line[1:-2]
             prop_dict = {'text': section, 'color': [0.8, 0, 0, 1]}
+            track_section = TrackSection(section)
+            self.sections[section.lower()] = track_section
+            self.current_section = track_section
+            self.current_subsection = None
         elif line.startswith('<'):
             subsection = line[1:-2]
             prop_dict = {'text': subsection, 'color': [0, 0.8, 0, 1]}
+            track_subsection = TrackSubSection(subsection)
+            self.subsections[subsection.lower()] = track_subsection
+            self.current_subsection = track_subsection
+            self.current_section.add_subsection(track_subsection)
         elif line.startswith('\\'):
             subsubsection = line[1:-2]
             prop_dict = {'text': subsubsection, 'color': [0, 0, 0.8, 1]}
@@ -86,7 +146,11 @@ class MusicList(TabbedPanelItem):
             track_name, track_url = line.split(':', 1)
             track_url = track_url.strip()
             prop_dict = {'text': track_name, 'track_url': track_url, 'color': [1, 1, 1, 1]}
-            self.tracks[track_name.lower()] = [track_name, track_url]
+            self.tracks[track_name.lower()] = [track_name, track_url, self.current_section, self.current_subsection]
+            if self.current_subsection is None:
+                self.current_section.add_track(track_name)
+            else:
+                self.current_subsection.add_track(track_name)
         prop_dict['size_hint_x'] = 1
         prop_dict['size_hint_y'] = None
         prop_dict['height'] = 30
@@ -100,42 +164,87 @@ class MusicList(TabbedPanelItem):
             self.clear_search()
         self.search_bar.text = ""
         Clock.schedule_once(self.refocus)
+        is_section = False
+        is_subsection = False
+        if target.startswith('['):
+            target = target.strip('[]')
+            is_section = True
+            self.search_space = self.section_search_space
+        elif target.startswith('<'):
+            target = target.strip('<>')
+            is_subsection = True
+            self.search_space = self.subsection_search_space
+        else:
+            self.search_space = self.track_search_space
         result = self.find_track(target)
         if result is None:
             return
         for track in result:
-            track_label = TrackLabel(size_hint_x=1, size_hint_y=None, height=30)
-            track_label.text = self.tracks[track.lower()][0]
-            track_label.track_url = self.tracks[track.lower()][1]
-            self.search_results.add_label(track_label)
+            if is_section and track in self.sections:
+                section = self.sections[track]
+                section_label = Label(size_hint_x=1, size_hint_y=None, height=30, color=[0.8, 0, 0, 1])
+                section_label.text = section.get_name()
+                self.search_results.add_label(section_label)
+                for section_track in section.get_tracks():
+                    self.add_track_to_search_result(section_track)
+                for subsection_name, subsection in section.get_subsections().items():
+                    subsection_label = Label(size_hint_x=1, size_hint_y=None, height=30, color=[0, 0.8, 0, 1])
+                    subsection_label.text = subsection.get_name()
+                    self.search_results.add_label(subsection_label)
+                    for track_name in subsection.get_tracks():
+                        self.add_track_to_search_result(track_name)
+            elif is_subsection and track in self.subsections:
+                subsection = self.subsections[track]
+                subsection_label = Label(size_hint_x=1, size_hint_y=None, height=30, color=[0, 0.8, 0, 1])
+                subsection_label.text = subsection.get_name()
+                self.search_results.add_label(subsection_label)
+                for track_name in subsection.get_tracks():
+                    self.add_track_to_search_result(track_name)
+            elif not is_subsection and not is_section:
+                track_section = self.tracks[track.lower()][2]
+                section_label = Label(size_hint_x=1, size_hint_y=None, height=30, color=[0.8, 0, 0, 1])
+                section_label.text = track_section.get_name()
+                self.search_results.add_label(section_label)
+                track_subsection = self.tracks[track.lower()][3]
+                if track_subsection is not None:
+                    subsection_label = Label(size_hint_x=1, size_hint_y=None, height=30, color=[0, 0.8, 0, 1])
+                    subsection_label.text = track_subsection.get_name()
+                    self.search_results.add_label(subsection_label)
+                self.add_track_to_search_result(track)
         layout = self.content
         layout.remove_widget(self.music_list_view)
         layout.add_widget(self.search_results, index=1)
 
+    def add_track_to_search_result(self, track):
+        track_label = TrackLabel(size_hint_x=1, size_hint_y=None, height=30)
+        track_label.text = self.tracks[track.lower()][0]
+        track_label.track_url = self.tracks[track.lower()][1]
+        self.search_results.add_label(track_label)
+
     def find_track(self, target):
-        found_index = binary_search(self.track_list, target)
+        found_index = binary_search(self.search_space, target)
         if found_index is None:
             return None
         self.search_done = True
         i = found_index
-        current_track = self.track_list[i].lower()
+        current_track = self.search_space[i].lower()
         result = []
         while current_track.startswith(target.lower()):
             result.append(current_track)
-            if i == len(self.track_list) - 1:
+            if i == len(self.search_space) - 1:
                 break
             i += 1
-            current_track = self.track_list[i].lower()
+            current_track = self.search_space[i].lower()
         i = found_index
         if i > 0:
             i -= 1
-            current_track = self.track_list[i].lower()
+            current_track = self.search_space[i].lower()
             while current_track.startswith(target.lower()):
                 result.append(current_track)
                 if i == 0:
                     break
                 i -= 1
-                current_track = self.track_list[i].lower()
+                current_track = self.search_space[i].lower()
         return result
 
     def refocus(self, *args):
