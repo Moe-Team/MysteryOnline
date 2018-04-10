479786f07d1bd754ba20a85618c8b178b2bb064f
Solomon <badsilverwolf@gmail.com>
2018-04-10 15:10:40 +0100

    MOPopup's button command arguments
    Can pass a list of arguments to be used by MOPopup's button command

--- src/mopopup.py
+++ src/mopopup.py
@@ -6,7 +6,7 @@ from kivy.uix.button import Button
 
 class MOPopup(Popup):
 
-    def __init__(self, title_, msg, btn_msg, dismissable=True, btn_command=None, *args, **kwargs):
+    def __init__(self, title_, msg, btn_msg, dismissable=True, btn_command=None, btn_command_args=[], *args, **kwargs):
         self.box_lay = BoxLayout(orientation='vertical')
         self.content = self.box_lay
         self.content.add_widget(Label(text=msg))
@@ -14,7 +14,7 @@ class MOPopup(Popup):
             btn = Button(text=btn_msg, size_hint=(1, 0.4))
             self.content.add_widget(btn)
             if dismissable:
-                btn_command = self.add_dismiss_to_command(btn_command)               
+                btn_command = self.add_dismiss_to_command(btn_command, *btn_command_args)               
             btn.bind(on_press=btn_command)
         else:
             self.auto_dismiss = False
@@ -23,12 +23,12 @@ class MOPopup(Popup):
         self.size = (400, 200)
         super(MOPopup, self).__init__(**kwargs)
 
-    def add_dismiss_to_command(self, btn_command):
+    def add_dismiss_to_command(self, btn_command, *args):
         if btn_command is None:
             return self.dismiss
         dismiss = self.dismiss
         def execute_and_dismiss(self):
-            btn_command()
+            btn_command(*args)
             dismiss()
         return execute_and_dismiss
 
