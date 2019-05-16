9087506127a7875f92a6d6e3374a46f3b1ac2634
Unknown <videoacta@gmail.com>
2019-05-16 17:31:52 +0200

    Resolution setting actually does something.
    Probably not good I can't really test

--- src/set_kivy_config.py
+++ src/set_kivy_config.py
@@ -1,8 +1,10 @@
-from kivy.config import Config
+from kivy.config import Config, ConfigParser
 
 Config.set('kivy', 'desktop', 1)
 Config.set('input', 'mouse', 'mouse,disable_multitouch')
 Config.set('kivy', 'exit_on_escape', 0)
 Config.set('kivy', 'window_icon', 'icon.ico')
-Config.set('graphics', 'width', 1366)
-Config.set('graphics', 'height', 768)
+config = ConfigParser()
+config.read('mysteryonline.ini')
+Config.set('graphics', 'width', int(config.get('display', 'resolution').split('x', 1)[0]))
+Config.set('graphics', 'height', int(config.get('display', 'resolution').split('x', 1)[1]))
