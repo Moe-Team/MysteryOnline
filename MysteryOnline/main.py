b2cf08903565412b519d1b11b4a7d1d580db6856
Unknown <videoacta@gmail.com>
2019-11-20 18:10:16 +0100

    OOC volume to 0 fix.

--- MysteryOnline/main.py
+++ MysteryOnline/main.py
@@ -358,6 +358,8 @@ class MysteryOnlineApp(App):
 
     def play_sound(self, sound: Sound, loop=False, volume=1.0):
         """Kivy is a mess, so we need to do this for *every* audio we want to play, on platforms other than windows."""
+        if volume == 0.00:
+            return
         if platform != "win":
             sound.load()
         sound.loop = loop
